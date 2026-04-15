fa99408781798831ab4792d81e8409a79c012e22
Michael <143526295+mgj0226@users.noreply.github.com>
2026-04-15 12:48:58 +0100

    Welcome Text

--- artgallery/src/App.js
+++ artgallery/src/App.js
@@ -5,18 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Welcome</h3>
+        <p>to my</p>
+        <h1>Art Gallery</h1>
       </header>
     </div>
   );
